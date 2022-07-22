95aa30ce26bfa1b621bdced3953935a375cfe774
danilkinkin <danilkinkin@gmail.com>
2022-07-22 22:15:09 +0400

    fix: fixed roundToDay and checkIsSameDay

--- app/src/main/java/com/danilkinkin/buckwheat/utils/time.kt
+++ app/src/main/java/com/danilkinkin/buckwheat/utils/time.kt
@@ -2,9 +2,7 @@ package com.danilkinkin.buckwheat.utils
 
 import java.text.SimpleDateFormat
 import java.util.*
-import kotlin.math.abs
 import kotlin.math.ceil
-import kotlin.math.floor
 
 const val DAY = 24 * 60 * 60 * 1000
 
@@ -13,13 +11,18 @@ val dateFormat = SimpleDateFormat("dd MMMM")
 val timeFormat = SimpleDateFormat("HH:mm")
 
 fun countDays(toDate: Date, fromDate: Date = Date()): Int {
-    val daysFrom = ceil(fromDate.time / DAY.toDouble()).toInt()
-    val daysTo = ceil(toDate.time / DAY.toDouble()).toInt()
+    val fromDateRound = roundToDay(fromDate)
+    val toDateRound = roundToDay(toDate)
+
+    val daysFrom = ceil(fromDateRound.time / DAY.toDouble()).toInt()
+    val daysTo = ceil(toDateRound.time / DAY.toDouble()).toInt()
 
     return daysTo - daysFrom
 }
 
-fun isSameDay(timestampA: Long, timestampB: Long): Boolean = floor(timestampA / DAY.toFloat()).toLong() == floor(timestampB / DAY.toFloat()).toLong()
+fun isSameDay(timestampA: Long, timestampB: Long): Boolean {
+    return roundToDay(Date(timestampA)) == roundToDay(Date(timestampB))
+}
 
 fun prettyDate(date: Date, showTime: Boolean = true, forceShowDate: Boolean = false): String {
     val currentYear = yearFormat.format(Date().time)
