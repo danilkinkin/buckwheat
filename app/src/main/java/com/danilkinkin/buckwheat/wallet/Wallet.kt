4795dbdd91e9de71d9d9e43e57a5e0fb6ca4d148
danilkinkin <danilkinkin@gmail.com>
2022-09-30 19:00:43 +0400

    fix: close keyboard when editing budget on press done key in keyboard

--- app/src/main/java/com/danilkinkin/buckwheat/wallet/Wallet.kt
+++ app/src/main/java/com/danilkinkin/buckwheat/wallet/Wallet.kt
@@ -2,6 +2,7 @@ package com.danilkinkin.buckwheat.wallet
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
@@ -11,9 +12,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.pluralStringResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -55,11 +58,12 @@ fun Wallet(
     val openCustomCurrencyEditorDialog = remember { mutableStateOf(false) }
     val openConfirmChangeBudgetDialog = remember { mutableStateOf(false) }
     val coroutineScope = rememberCoroutineScope()
+    val keyboardController = LocalSoftwareKeyboardController.current
 
-    val navigationBarHeight = androidx.compose.ui.unit.max(
-        WindowInsets.systemBars.asPaddingValues().calculateBottomPadding(),
-        16.dp,
-    )
+    val navigationBarHeight = WindowInsets.systemBars
+        .asPaddingValues()
+        .calculateBottomPadding()
+        .coerceAtLeast(16.dp)
 
     Surface {
         Column(modifier = Modifier.padding(bottom = navigationBarHeight)) {
@@ -99,11 +103,17 @@ fun Wallet(
                         unfocusedIndicatorColor = Color.Transparent,
                         errorIndicatorColor = Color.Transparent,
                     ),
-                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Decimal),
                     textStyle = MaterialTheme.typography.displaySmall,
                     visualTransformation = visualTransformationAsCurrency(
                         currency = ExtendCurrency(type = CurrencyType.NONE),
                         hintColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.2f),
+                    ),
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Decimal,
+                        imeAction = ImeAction.Done,
+                    ),
+                    keyboardActions = KeyboardActions(
+                        onDone = {keyboardController?.hide()}
                     )
                 )
                 Divider()
