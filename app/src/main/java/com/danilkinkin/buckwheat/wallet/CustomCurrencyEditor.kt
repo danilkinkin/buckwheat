c7ed1d0a561ea06b9d455c2b3fa521b68a560b98
danilkinkin <danilkinkin@gmail.com>
2022-10-06 23:37:25 +0400

    fix: fixed length and keyboard in custom currency editor

--- app/src/main/java/com/danilkinkin/buckwheat/wallet/CustomCurrencyEditor.kt
+++ app/src/main/java/com/danilkinkin/buckwheat/wallet/CustomCurrencyEditor.kt
@@ -2,13 +2,15 @@ package com.danilkinkin.buckwheat.wallet
 
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
@@ -16,6 +18,7 @@ import androidx.compose.ui.window.DialogProperties
 import com.danilkinkin.buckwheat.R
 import com.danilkinkin.buckwheat.ui.BuckwheatTheme
 import com.danilkinkin.buckwheat.util.combineColors
+import java.lang.Integer.min
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -24,7 +27,7 @@ fun CustomCurrencyEditorContent(
     onChange: (currency: String) -> Unit,
     onClose: () -> Unit,
 ) {
-    val selectCurrency = remember { mutableStateOf(defaultCurrency ?: "") }
+    var selectCurrency by remember { mutableStateOf(defaultCurrency ?: "") }
 
     Card(
         shape = MaterialTheme.shapes.extraLarge,
@@ -43,8 +46,14 @@ fun CustomCurrencyEditorContent(
                     modifier = Modifier
                         .fillMaxWidth()
                         .padding(horizontal = 16.dp),
-                    value = selectCurrency.value,
-                    onValueChange = { selectCurrency.value = it },
+                    value = selectCurrency,
+                    onValueChange = {
+                        val string = it
+                            .trim()
+                            .replace("\r|\n","")
+
+                        selectCurrency = string.substring(0, min(4, string.length))
+                    },
                     shape = TextFieldDefaults.filledShape,
                     colors = TextFieldDefaults.textFieldColors(
                         containerColor = combineColors(
@@ -52,6 +61,18 @@ fun CustomCurrencyEditorContent(
                             MaterialTheme.colorScheme.surface,
                             0.5F
                         )
+                    ),
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Text,
+                        imeAction = ImeAction.Go,
+                    ),
+                    keyboardActions = KeyboardActions(
+                        onGo = {
+                            if (selectCurrency.isEmpty()) return@KeyboardActions
+
+                            onChange(selectCurrency)
+                            onClose()
+                        }
                     )
                 )
             }
@@ -70,12 +91,12 @@ fun CustomCurrencyEditorContent(
                 }
                 Button(
                     onClick = {
-                        onChange(selectCurrency.value!!)
+                        onChange(selectCurrency)
                         onClose()
                     },
                     colors = ButtonDefaults.textButtonColors(),
                     contentPadding = ButtonDefaults.TextButtonContentPadding,
-                    enabled = selectCurrency.value.trim() !== "",
+                    enabled = selectCurrency.trim() !== "",
                 ) {
                     Text(text = stringResource(R.string.accept))
                 }
