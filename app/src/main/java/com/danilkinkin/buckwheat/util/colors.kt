0efef3d4a19732e30fb3ee1c7ea68f404417309f
danilkinkin <danilkinkin@gmail.com>
2022-10-09 20:22:46 +0400

    feat: added method for combine array colors

--- app/src/main/java/com/danilkinkin/buckwheat/util/colors.kt
+++ app/src/main/java/com/danilkinkin/buckwheat/util/colors.kt
@@ -1,6 +1,8 @@
 package com.danilkinkin.buckwheat.util
 
 import androidx.compose.ui.graphics.Color
+import kotlin.math.ceil
+import kotlin.math.floor
 
 fun combineColors(colorA: Color, colorB: Color, angle: Float = 0.5F): Color {
     val colorAPart = (1F - angle) * 2
@@ -11,4 +13,12 @@ fun combineColors(colorA: Color, colorB: Color, angle: Float = 0.5F): Color {
         green = (colorA.green * colorAPart + colorB.green * colorBPart) / 2,
         blue = (colorA.blue * colorAPart + colorB.blue * colorBPart) / 2,
     )
+}
+
+fun combineColors(colors: List<Color>, angle: Float = 0.5F): Color {
+    val approximateIndex = (colors.size - 1) * angle
+    val colorA = colors[floor(approximateIndex).toInt()]
+    val colorB = colors[ceil(approximateIndex).toInt()]
+
+    return combineColors(colorA, colorB, approximateIndex - floor(approximateIndex))
 }
