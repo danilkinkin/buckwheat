d443ca65a59fcd4840e46277d70bcfa6b746583d
danilkinkin <danilkinkin@gmail.com>
2022-10-19 22:26:55 +0400

    fix: fixed role day button in calendar

--- app/src/main/java/com/danilkinkin/buckwheat/calendar/Day.kt
+++ app/src/main/java/com/danilkinkin/buckwheat/calendar/Day.kt
@@ -2,17 +2,20 @@ package com.danilkinkin.buckwheat.calendar
 
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import com.danilkinkin.buckwheat.calendar.model.CalendarUiState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -38,6 +41,7 @@ internal fun DayOfWeekHeading(day: String, modifier: Modifier = Modifier) {
 private fun DayContainer(
     modifier: Modifier = Modifier,
     current: Boolean = false,
+    disabled: Boolean = false,
     onSelect: () -> Unit = { },
     content: @Composable () -> Unit
 ) {
@@ -46,13 +50,17 @@ private fun DayContainer(
             .height(CELL_SIZE)
             .widthIn(min = CELL_SIZE)
             .fillMaxWidth()
-            .background(Color.Transparent,
-            )
-            .pointerInput(Any()) {
-                detectTapGestures {
-                    onSelect()
-                }
-            },
+            .background(Color.Transparent)
+            .clickable(
+                onClick = { onSelect() },
+                enabled = !disabled,
+                role = Role.Button,
+                interactionSource = remember { MutableInteractionSource() },
+                indication = rememberRipple(
+                    bounded = false,
+                    radius = CELL_SIZE / 2,
+                )
+            ),
         contentAlignment = Alignment.Center
     ) {
         if (current) {
@@ -89,7 +97,8 @@ internal fun Day(
     DayContainer(
         modifier = modifier,
         current = current,
-        onSelect = { if (!disabled) onDayClicked(day) },
+        disabled = disabled,
+        onSelect = { onDayClicked(day) },
     ) {
 
         Text(
