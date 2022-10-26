744deb434e94badf5c5c0419b5264e31c304d4cc
danilkinkin <danilkinkin@gmail.com>
2022-10-27 00:32:27 +0400

    bump: update app version to beta-5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.danilkinkin.buckwheat"
         minSdk = 26
         targetSdk = 33
-        versionCode = 4
-        versionName = "alpha-4.0"
+        versionCode = 5
+        versionName = "beta-1.0"
         testInstrumentationRunner = "com.danilkinkin.buckwheat.CustomTestRunner"
         manifestPlaceholders["sentryDsn"] = ""
         javaCompileOptions {
