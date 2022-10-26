9055b5054fdfc283794605a68ae1a175873883ae
danilkinkin <danilkinkin@gmail.com>
2022-10-26 23:20:39 +0400

    fix: refactor confirm change budget dialog

--- app/src/main/java/com/danilkinkin/buckwheat/wallet/ConfirmChangeBudget.kt
+++ app/src/main/java/com/danilkinkin/buckwheat/wallet/ConfirmChangeBudget.kt
@@ -4,9 +4,12 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
@@ -23,19 +26,33 @@ fun ConfirmChangeBudget(
     Card(
         shape = MaterialTheme.shapes.extraLarge,
         modifier = Modifier
-            .widthIn(max = 500.dp)
+            .widthIn(max = 440.dp)
             .padding(36.dp)
     ) {
         Column {
+            Box(
+                modifier = Modifier.fillMaxWidth().padding(top = 24.dp, bottom = 16.dp),
+                contentAlignment = Alignment.Center,
+            ) {
+                Icon(
+                    painter = painterResource(R.drawable.ic_priority_high),
+                    tint = LocalContentColor.current.copy(alpha = 0.7f),
+                    contentDescription = null,
+                )
+            }
             Text(
                 text = stringResource(R.string.confirm_change_budget_title),
                 style = MaterialTheme.typography.titleLarge,
-                modifier = Modifier.padding(24.dp)
+                modifier = Modifier
+                    .padding(horizontal = 24.dp)
+                    .padding(bottom = 24.dp)
+                    .fillMaxWidth(),
+                textAlign = TextAlign.Center,
             )
             Text(
                 text = stringResource(R.string.confirm_change_budget_description),
                 style = MaterialTheme.typography.bodyMedium,
-                modifier = Modifier.padding(horizontal = 24.dp)
+                modifier = Modifier.padding(horizontal = 24.dp),
             )
             Row(
                 horizontalArrangement = Arrangement.End,
