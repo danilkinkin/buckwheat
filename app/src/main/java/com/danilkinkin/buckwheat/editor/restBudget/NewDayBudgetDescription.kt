f7901847e07f25d4e16a9776c4fea84d995ebfbf
danilkinkin <danilkinkin@gmail.com>
2022-11-17 21:56:01 +0400

    fix: refactor packages in editor

--- app/src/main/java/com/danilkinkin/buckwheat/editor/restBudget/NewDayBudgetDescription.kt
+++ app/src/main/java/com/danilkinkin/buckwheat/editor/restBudget/NewDayBudgetDescription.kt
@@ -1,38 +1,27 @@
-package com.danilkinkin.buckwheat.editor
+package com.danilkinkin.buckwheat.editor.restBudget
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
 import com.danilkinkin.buckwheat.R
-import com.danilkinkin.buckwheat.base.ButtonRow
-import com.danilkinkin.buckwheat.data.SpendsViewModel
 import com.danilkinkin.buckwheat.ui.BuckwheatTheme
 
-const val BUDGET_IS_OVER_DESCRIPTION_SHEET = "budgetIsOverDescription"
+const val NEW_DAY_BUDGET_DESCRIPTION_SHEET = "newDayBudgetDescription"
 
 @Composable
-fun BudgetIsOverDescription(
-    spendsViewModel: SpendsViewModel = hiltViewModel(),
-    onClose: () -> Unit = {},
-) {
+fun NewDayBudgetDescription(onClose: () -> Unit = {}) {
     val navigationBarHeight = androidx.compose.ui.unit.max(
         WindowInsets.systemBars.asPaddingValues().calculateBottomPadding(),
         16.dp,
     )
-
-    val overspendingWarnHidden by spendsViewModel.overspendingWarnHidden.observeAsState(false)
 
     Surface {
         Column(modifier = Modifier.padding(bottom = navigationBarHeight)) {
@@ -43,37 +32,18 @@ fun BudgetIsOverDescription(
                 contentAlignment = Alignment.Center
             ) {
                 Text(
-                    text = stringResource(R.string.budget_end),
+                    text = stringResource(R.string.new_daily_budget),
                     style = MaterialTheme.typography.titleLarge,
                 )
             }
 
             Text(
                 modifier = Modifier.padding(horizontal = 24.dp).fillMaxWidth(),
-                text = stringResource(R.string.budget_end_description),
+                text = stringResource(R.string.new_daily_budget_description),
                 textAlign = TextAlign.Center,
                 style = MaterialTheme.typography.bodyMedium,
             )
-
-            Spacer(modifier = Modifier.height(32.dp))
-
-            ButtonRow(
-                text = stringResource(R.string.hide_overspending_warn),
-                description = stringResource(R.string.hide_overspending_warn_description),
-                onClick = {
-                    spendsViewModel.hideOverspendingWarn(!overspendingWarnHidden)
-                },
-                endContent = {
-                    Switch(
-                        checked = overspendingWarnHidden,
-                        onCheckedChange = {
-                            spendsViewModel.hideOverspendingWarn(!overspendingWarnHidden)
-                        },
-                    )
-
-                }
-            )
-
+            
             Spacer(modifier = Modifier.height(32.dp))
         }
     }
@@ -83,6 +53,6 @@ fun BudgetIsOverDescription(
 @Composable
 private fun PreviewDefault() {
     BuckwheatTheme {
-        BudgetIsOverDescription()
+        NewDayBudgetDescription()
     }
 }
--- app/src/main/java/com/danilkinkin/buckwheat/editor/toolbar/DebugMenu.kt
+++ app/src/main/java/com/danilkinkin/buckwheat/editor/toolbar/DebugMenu.kt
@@ -1,4 +1,4 @@
-package com.danilkinkin.buckwheat.editor
+package com.danilkinkin.buckwheat.editor.toolbar
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
