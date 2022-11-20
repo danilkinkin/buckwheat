655f48cd3d050ef64ba6b25d9ba646f3e5f2ebb7
danilkinkin <danilkinkin@gmail.com>
2022-11-21 00:48:35 +0400

    fix: fixed change locale

--- app/src/main/java/com/danilkinkin/buckwheat/settings/LangSwitcher.kt
+++ app/src/main/java/com/danilkinkin/buckwheat/settings/LangSwitcher.kt
@@ -1,35 +1,90 @@
 package com.danilkinkin.buckwheat.settings
 
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Surface
+import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.Surface
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat.startActivity
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.danilkinkin.buckwheat.R
+import com.danilkinkin.buckwheat.appLocale
+import com.danilkinkin.buckwheat.base.ButtonRow
 import com.danilkinkin.buckwheat.base.CheckedRow
-import com.danilkinkin.buckwheat.base.TextRow
+import com.danilkinkin.buckwheat.base.Divider
+import com.danilkinkin.buckwheat.data.AppViewModel
+import com.danilkinkin.buckwheat.data.PathState
 import com.danilkinkin.buckwheat.ui.BuckwheatTheme
+import com.danilkinkin.buckwheat.util.titleCase
 import kotlinx.coroutines.launch
 import switchLocale
-import java.util.Locale
+import java.util.*
+
+const val SETTINGS_CHANGE_LOCALE_SHEET = "settings.changeLocale"
 
 @Composable
-fun LangSwitcher() {
+fun LangSwitcher(appViewModel: AppViewModel = hiltViewModel()) {
     val context = LocalContext.current
     val configuration = LocalConfiguration.current
+    val currentLocale = configuration.locales[0]
+
+    ButtonRow(
+        icon = painterResource(R.drawable.ic_language),
+        text = stringResource(R.string.locale_label),
+        onClick = {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                val intent = Intent(android.provider.Settings.ACTION_APP_LOCALE_SETTINGS)
+                val uri: Uri = Uri.fromParts("package", context.packageName, null)
+                intent.data = uri
+                startActivity(context, intent, null)
+            } else {
+                appViewModel.openSheet(PathState(SETTINGS_CHANGE_LOCALE_SHEET))
+            }
+        },
+        endContent = {
+            Text(
+                modifier = Modifier.padding(end = 8.dp),
+                text = currentLocale.displayName.titleCase(),
+                style = MaterialTheme.typography.labelMedium,
+                color = LocalContentColor.current.copy(alpha = 0.6f),
+            )
+        }
+    )
+}
+
+@Composable
+fun LangSwitcherDialog(onClose: () -> Unit) {
+    val context = LocalContext.current
+    val configuration = LocalConfiguration.current
+    val overrideLocale = LocalContext.current.appLocale
     val coroutineScope = rememberCoroutineScope()
 
-    fun handleSwitchLang(localeCode: String) {
+    val navigationBarHeight = androidx.compose.ui.unit.max(
+        WindowInsets.systemBars.asPaddingValues().calculateBottomPadding(),
+        16.dp,
+    )
+
+    fun handleSwitchLang(localeCode: String?) {
         coroutineScope.launch {
             switchLocale(
                 context,
                 configuration,
                 localeCode
             )
+            onClose()
         }
     }
 
@@ -42,17 +97,32 @@ fun LangSwitcher() {
         Locale("sv"),
     )
 
-    Column {
-        TextRow(
-            icon = painterResource(R.drawable.ic_language),
-            text = stringResource(R.string.locale_label),
-        )
-        locales.forEach { locale ->
+    Surface {
+        Column(modifier = Modifier.padding(bottom = navigationBarHeight)) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(24.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = stringResource(R.string.locale_label),
+                    style = MaterialTheme.typography.titleLarge,
+                )
+            }
+            Divider()
             CheckedRow(
-                checked = locale.language === currentLocale.language,
-                onValueChange = { handleSwitchLang(locale.language) },
-                text = locale.displayName,
+                text = stringResource(R.string.locale_system),
+                checked = overrideLocale === null,
+                onValueChange = { handleSwitchLang(null) },
             )
+            locales.forEach { locale ->
+                CheckedRow(
+                    text = locale.getDisplayName(locale).titleCase(),
+                    checked = overrideLocale !== null && locale.language === currentLocale.language,
+                    onValueChange = { handleSwitchLang(locale.language) },
+                )
+            }
         }
     }
 }
