a8731479bf1464dfc7f27445cb65f025200e70d2
danilkinkin <danilkinkin@gmail.com>
2023-02-14 01:47:31 +0400

    feat: fixed widget layout

--- app/src/main/java/com/danilkinkin/buckwheat/AppWidget.kt
+++ app/src/main/java/com/danilkinkin/buckwheat/AppWidget.kt
@@ -1,22 +1,15 @@
 package com.danilkinkin.buckwheat
 
 
-import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import android.graphics.LightingColorFilter
 import android.util.Log
-import android.widget.RemoteViews
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.core.content.ContextCompat
-import androidx.core.content.res.ResourcesCompat
-import androidx.core.graphics.drawable.toBitmap
 import androidx.glance.*
 import androidx.glance.action.ActionParameters
-import androidx.glance.action.actionStartActivity
 import androidx.glance.action.clickable
 import androidx.glance.appwidget.*
 import androidx.glance.appwidget.action.ActionCallback
@@ -28,7 +21,8 @@ import androidx.glance.text.TextStyle
 import androidx.glance.unit.ColorProvider
 import com.danilkinkin.buckwheat.util.ExtendCurrency
 import com.danilkinkin.buckwheat.util.prettyCandyCanes
-import androidx.glance.appwidget.action.actionStartActivity
+import com.danilkinkin.buckwheat.widget.AddSpentButton
+import com.danilkinkin.buckwheat.widget.Label
 import java.math.BigDecimal
 
 
@@ -39,11 +33,12 @@ class AppWidget : GlanceAppWidget() {
         private val smallMode = DpSize(200.dp, 100.dp)
         private val mediumMode = DpSize(200.dp, 130.dp)
         private val largeMode = DpSize(200.dp, 190.dp)
-        private val hugeMode = DpSize(200.dp, 420.dp)
+        private val hugeMode = DpSize(200.dp, 320.dp)
+        private val superHugeMode = DpSize(200.dp, 460.dp)
     }
 
     override val sizeMode: SizeMode = SizeMode.Responsive(
-        setOf(superTinyMode, tinyMode, smallMode, mediumMode, largeMode, hugeMode)
+        setOf(superTinyMode, tinyMode, smallMode, mediumMode, largeMode, hugeMode, superHugeMode)
     )
 
     @Composable
@@ -56,117 +51,107 @@ class AppWidget : GlanceAppWidget() {
         //actionStartActivity(intent)
 
         val isTiny = size == superTinyMode || size == tinyMode
+        val isHugeAndUpSize = size == hugeMode || size == superHugeMode
 
         val verticalPadding = if (size == superTinyMode) 0.dp else 12.dp
 
-        Box(
-            modifier = GlanceModifier
-                .clickable(actionRunCallback<AddSpendActionCallback>())
-                .fillMaxSize()
-                .background(R.color.material_dynamic_neutral99)
-                .cornerRadius(24.dp)
+        Column(
+            modifier = GlanceModifier.clickable(actionRunCallback<AddSpendActionCallback>())
+                .fillMaxSize().background(R.color.material_dynamic_secondary95).cornerRadius(24.dp)
         ) {
-            Column(
-                modifier = GlanceModifier.fillMaxSize().padding(12.dp, 0.dp),
-                verticalAlignment = if (isTiny) Alignment.CenterVertically else Alignment.Top
+            Box(
+                modifier = GlanceModifier.fillMaxWidth()
+                    .then(if (isHugeAndUpSize) GlanceModifier.wrapContentHeight() else GlanceModifier.fillMaxHeight())
             ) {
-                Text(
-                    modifier = GlanceModifier.padding(8.dp, verticalPadding, 8.dp, 0.dp),
-                    text = prettyCandyCanes(BigDecimal("10000"), ExtendCurrency.getInstance("USD")),
-                    style = TextStyle(
-                        color = ColorProvider(R.color.material_dynamic_primary10),
-                        fontWeight = FontWeight.Bold,
-                        fontSize = when (size) {
-                            hugeMode -> 56.sp
-                            superTinyMode -> 24.sp
-                            else -> 36.sp
-                        },
-                    )
-                )
-                Text(
-                    modifier = GlanceModifier.padding(8.dp, 0.dp, 8.dp, verticalPadding + 6.dp),
-                    text = context.resources.getString(R.string.budget_for_today),
-                    style = TextStyle(
-                        color = ColorProvider(R.color.material_dynamic_neutral40),
-                        fontWeight = FontWeight.Normal,
-                        fontSize = 11.sp,
-                    )
-                )
-                if (size == largeMode || size == hugeMode) {
+                Column(
+                    modifier = GlanceModifier.fillMaxWidth()
+                        .then(if (isHugeAndUpSize) GlanceModifier.wrapContentHeight() else GlanceModifier.fillMaxHeight())
+                        .padding(12.dp, 0.dp),
+                    verticalAlignment = if (isTiny) Alignment.CenterVertically else Alignment.Top
+                ) {
                     Text(
                         modifier = GlanceModifier.padding(8.dp, verticalPadding, 8.dp, 0.dp),
-                        text = prettyCandyCanes(BigDecimal("698"), ExtendCurrency.getInstance("USD")),
+                        text = prettyCandyCanes(
+                            BigDecimal("10000"), ExtendCurrency.getInstance("USD")
+                        ),
                         style = TextStyle(
-                            color = ColorProvider(R.color.material_dynamic_primary10),
+                            color = ColorProvider(R.color.material_dynamic_primary40),
                             fontWeight = FontWeight.Bold,
-                            fontSize = 36.sp,
+                            fontSize = when (size) {
+                                superHugeMode -> 56.sp
+                                superTinyMode -> 24.sp
+                                else -> 36.sp
+                            },
                         )
                     )
-                    Text(
+                    Label(
                         modifier = GlanceModifier.padding(8.dp, 0.dp, 8.dp, verticalPadding + 6.dp),
-                        text = context.resources.getString(R.string.today),
-                        style = TextStyle(
-                            color = ColorProvider(R.color.material_dynamic_neutral40),
-                            fontWeight = FontWeight.Normal,
-                            fontSize = 11.sp,
-                        )
+                        text = context.resources.getString(R.string.budget_for_today),
                     )
+                    if (size == largeMode || isHugeAndUpSize) {
+                        Text(
+                            modifier = GlanceModifier.padding(8.dp, verticalPadding, 8.dp, 0.dp),
+                            text = prettyCandyCanes(
+                                BigDecimal("698"), ExtendCurrency.getInstance("USD")
+                            ),
+                            style = TextStyle(
+                                color = ColorProvider(R.color.material_dynamic_secondary20),
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 36.sp,
+                            )
+                        )
+                        Label(
+                            modifier = GlanceModifier.padding(
+                                8.dp, 0.dp, 8.dp, verticalPadding + 6.dp
+                            ),
+                            text = context.resources.getString(R.string.today),
+                        )
+                    }
+                }
+                if (!isTiny) {
+                    Column(
+                        modifier = if (!isHugeAndUpSize) GlanceModifier.fillMaxSize() else GlanceModifier.fillMaxWidth(),
+                        horizontalAlignment = Alignment.End,
+                        verticalAlignment = Alignment.Bottom,
+                    ) {
+                        AddSpentButton()
+                    }
                 }
             }
-            if (!isTiny) {
+            if (isHugeAndUpSize) {
                 Column(
-                    modifier = GlanceModifier.fillMaxSize(),
-                    horizontalAlignment = Alignment.End,
-                    verticalAlignment = Alignment.Bottom,
+                    modifier = GlanceModifier.fillMaxSize().padding(8.dp, 0.dp, 8.dp, 8.dp)
                 ) {
-                    AddSpentButton()
+                    Column(
+                        modifier = GlanceModifier
+                            .then(if (size == superHugeMode) GlanceModifier.fillMaxWidth().height(120.dp) else GlanceModifier.fillMaxSize())
+                            .cornerRadius(20.dp)
+                            .background(R.color.material_dynamic_secondary90)
+                    ) {
+                        Text("History")
+                    }
+
+                    if (size == superHugeMode) {
+                        Spacer(GlanceModifier.height(8.dp))
+                        Column(
+                            modifier = GlanceModifier
+                                .then(if (size == superHugeMode) GlanceModifier.fillMaxWidth().defaultWeight() else GlanceModifier.fillMaxSize())
+                                .cornerRadius(20.dp)
+                                .background(R.color.material_dynamic_secondary90)
+                        ) {
+                            Text("History")
+                        }
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun AddSpentButton() {
-    val context = LocalContext.current
-    val color = R.color.material_dynamic_primary40
-
-    Row(
-        modifier = GlanceModifier.padding(16.dp),
-        verticalAlignment = Alignment.CenterVertically,
-    ) {
-        Text(
-            text = context.resources.getString(R.string.add_spent),
-            style = TextStyle(
-                color = ColorProvider(color),
-                fontWeight = FontWeight.Bold,
-                fontSize = 14.sp,
-            )
-        )
-        Spacer(modifier = GlanceModifier.width(8.dp))
-
-        val drawable = ResourcesCompat.getDrawable(
-            context.resources,
-            R.drawable.ic_add,
-            null,
-        )!!
-
-        val iconColor = ContextCompat.getColor(context, color)
-        drawable.colorFilter = LightingColorFilter(iconColor, iconColor)
-
-        Image(
-            modifier = GlanceModifier.size(24.dp),
-            provider = ImageProvider(drawable.toBitmap()),
-            contentDescription = null,
-        )
-    }
-}
 
 class AddSpendActionCallback : ActionCallback {
     override suspend fun onAction(
-        context: Context,
-        glanceId: GlanceId,
-        parameters: ActionParameters
+        context: Context, glanceId: GlanceId, parameters: ActionParameters
     ) {
         Log.d("Buckwheat widget", "add spend...")
     }
