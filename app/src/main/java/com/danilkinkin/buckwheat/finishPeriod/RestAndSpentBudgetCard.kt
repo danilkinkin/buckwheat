51492da1e159ed5fb5e2c6fe26f7e7848017610c
danilkinkin <d.zahvatkin@ticketscloud.com>
2023-04-09 00:52:59 +0400

    fix: merge rest and spent budget cards

--- app/src/main/java/com/danilkinkin/buckwheat/finishPeriod/RestAndSpentBudgetCard.kt
+++ app/src/main/java/com/danilkinkin/buckwheat/finishPeriod/RestAndSpentBudgetCard.kt
@@ -1,20 +1,48 @@
 package com.danilkinkin.buckwheat.finishPeriod
 
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.FloatTweenSpec
+import androidx.compose.animation.core.LinearEasing
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Outline
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.PathOperation
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.danilkinkin.buckwheat.R
 import com.danilkinkin.buckwheat.data.AppViewModel
 import com.danilkinkin.buckwheat.data.SpendsViewModel
-import com.danilkinkin.buckwheat.util.ExtendCurrency
+import com.danilkinkin.buckwheat.ui.BuckwheatTheme
+import com.danilkinkin.buckwheat.ui.colorBad
+import com.danilkinkin.buckwheat.ui.colorGood
+import com.danilkinkin.buckwheat.ui.colorNotGood
+import com.danilkinkin.buckwheat.util.*
+import kotlinx.coroutines.launch
+import java.math.BigDecimal
+import java.math.RoundingMode
+import java.text.NumberFormat
+import java.util.*
+import kotlin.math.ceil
 
 @Composable
 fun RestAndSpentBudgetCard(
@@ -28,24 +56,105 @@ fun RestAndSpentBudgetCard(
     val wholeBudget = spendsViewModel.budget.value!!
     val restBudget = spendsViewModel.calcResetBudget()
 
+    val percent = remember {
+        if (showRestBudgetCard) {
+            restBudget.divide(wholeBudget, 5, RoundingMode.HALF_EVEN)
+        } else {
+            BigDecimal(1).minus((wholeBudget - restBudget).divide(wholeBudget, 5, RoundingMode.HALF_EVEN))
+        }
+    }
+
+    val percentFormatted = remember {
+        val formatter = NumberFormat.getNumberInstance(Locale.getDefault())
+        formatter.maximumFractionDigits = 2
+        formatter.minimumFractionDigits = 0
+
+        if (showRestBudgetCard) {
+            formatter.format(percent.multiply(BigDecimal(100)))
+        } else {
+            formatter.format(BigDecimal(1).minus(percent).multiply(BigDecimal(100)))
+        }
+    }
+
+    val shift = remember { Animatable(0f) }
+    val coroutineScope = rememberCoroutineScope()
+
+    LaunchedEffect(Unit) {
+        fun anim() {
+            coroutineScope.launch {
+                shift.animateTo(
+                    1f,
+                    animationSpec = FloatTweenSpec(4000, 0, LinearEasing)
+                )
+                shift.snapTo(0f)
+                anim()
+            }
+        }
+
+        anim()
+    }
+
+    val harmonizedColor = toPalette(
+        harmonize(
+            combineColors(
+                listOf(
+                    colorBad,
+                    colorNotGood,
+                    colorGood,
+                ),
+                percent.coerceAtLeast(BigDecimal(0)).toFloat(),
+            )
+        )
+    )
+
     Box(
         modifier = modifier
             .clip(shape = MaterialTheme.shapes.extraLarge)
             .clickable { appViewModel.setShowRestBudgetCardByDefault(!showRestBudgetCard) }
     ) {
-        if (showRestBudgetCard) {
-            RestBudgetCard(
-                rest = restBudget,
-                budget = wholeBudget,
-                currency = currency!!,
-            )
-        } else {
-            SpendsBudgetCard(
-                spend = wholeBudget - restBudget,
-                budget = wholeBudget,
-                currency = currency!!,
-            )
-        }
+        StatCard(
+            modifier = modifier,
+            colors = CardDefaults.cardColors(
+                containerColor = harmonizedColor.container,
+                contentColor = harmonizedColor.onContainer,
+            ),
+            value = prettyCandyCanes(
+                if (showRestBudgetCard) {
+                    restBudget
+                } else {
+                    wholeBudget - restBudget
+                },
+                currency = currency,
+            ),
+            label = if (showRestBudgetCard) {
+                stringResource(R.string.rest_budget)
+            } else {
+                stringResource(R.string.spent_budget)
+            },
+            content = {
+                Spacer(modifier = Modifier.height(6.dp))
+                Text(
+                    text = stringResource(R.string.rest_budget_percent, percentFormatted),
+                    style = MaterialTheme.typography.bodyMedium,
+                    fontWeight = MaterialTheme.typography.bodyLarge.fontWeight,
+                )
+            },
+            backdropContent = {
+                Box(
+                    modifier = Modifier
+                        .background(
+                            harmonizedColor.main,
+                            shape = WavyShape(
+                                period = 30.dp,
+                                amplitude = 2.dp,
+                                shift = shift.value,
+                            ),
+                        )
+                        .fillMaxHeight()
+                        .fillMaxWidth(percent.toFloat()),
+                )
+            }
+        )
         Row(
             modifier = Modifier
                 .padding(16.dp)
@@ -69,5 +178,62 @@ fun RestAndSpentBudgetCard(
                     )
             )
         }
+    }
+}
+
+class WavyShape(
+    private val period: Dp,
+    private val amplitude: Dp,
+    private val shift: Float,
+) : Shape {
+    override fun createOutline(
+        size: Size,
+        layoutDirection: LayoutDirection,
+        density: Density,
+    ) = Outline.Generic(Path().apply {
+        val halfPeriod = with(density) { period.toPx() } / 2
+        val amplitude = with(density) { amplitude.toPx() }
+
+        val wavyPath = Path().apply {
+            moveTo(x = 0f, y = 0f)
+            lineTo(size.width - amplitude, -halfPeriod * 2.5f + halfPeriod * 2 * shift)
+            repeat(ceil(size.height / halfPeriod + 3).toInt()) { i ->
+                relativeQuadraticBezierTo(
+                    dx1 = 2 * amplitude * (if (i % 2 == 0) 1 else -1),
+                    dy1 = halfPeriod / 2,
+                    dx2 = 0f,
+                    dy2 = halfPeriod,
+                )
+            }
+            lineTo(0f, size.height)
+        }
+        val boundsPath = Path().apply {
+            addRect(Rect(offset = Offset.Zero, size = size))
+        }
+        op(wavyPath, boundsPath, PathOperation.Intersect)
+    })
+}
+
+@Preview(name = "The budget is almost completely spent")
+@Composable
+private fun Preview() {
+    BuckwheatTheme {
+        RestAndSpentBudgetCard()
+    }
+}
+
+@Preview(name = "Budget half spent")
+@Composable
+private fun PreviewHalf() {
+    BuckwheatTheme {
+        RestAndSpentBudgetCard()
+    }
+}
+
+@Preview(name = "Almost no budget")
+@Composable
+private fun PreviewFull() {
+    BuckwheatTheme {
+        RestAndSpentBudgetCard()
     }
 }
