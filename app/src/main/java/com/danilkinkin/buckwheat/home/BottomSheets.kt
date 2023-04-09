a90e15a8dabc5aa10cf555b67415545691b00df2
danilkinkin <d.zahvatkin@ticketscloud.com>
2023-04-09 14:29:27 +0400

    fix: remove old daily budget info

--- app/src/main/java/com/danilkinkin/buckwheat/home/BottomSheets.kt
+++ app/src/main/java/com/danilkinkin/buckwheat/home/BottomSheets.kt
@@ -14,10 +14,10 @@ import com.danilkinkin.buckwheat.data.AppViewModel
 import com.danilkinkin.buckwheat.data.PathState
 import com.danilkinkin.buckwheat.data.SpendsViewModel
 import com.danilkinkin.buckwheat.editor.*
-import com.danilkinkin.buckwheat.editor.restBudget.BUDGET_IS_OVER_DESCRIPTION_SHEET
-import com.danilkinkin.buckwheat.editor.restBudget.BudgetIsOverDescription
-import com.danilkinkin.buckwheat.editor.restBudget.NEW_DAY_BUDGET_DESCRIPTION_SHEET
-import com.danilkinkin.buckwheat.editor.restBudget.NewDayBudgetDescription
+import com.danilkinkin.buckwheat.editor.toolbar.restBudgetPill.BUDGET_IS_OVER_DESCRIPTION_SHEET
+import com.danilkinkin.buckwheat.editor.toolbar.restBudgetPill.BudgetIsOverDescription
+import com.danilkinkin.buckwheat.editor.toolbar.restBudgetPill.NEW_DAY_BUDGET_DESCRIPTION_SHEET
+import com.danilkinkin.buckwheat.editor.toolbar.restBudgetPill.NewDayBudgetDescription
 import com.danilkinkin.buckwheat.editor.toolbar.DEBUG_MENU_SHEET
 import com.danilkinkin.buckwheat.editor.toolbar.DebugMenu
 import com.danilkinkin.buckwheat.effects.Confetti
