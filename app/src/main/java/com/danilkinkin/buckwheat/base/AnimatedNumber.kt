bfd18d2eee14f4e2ba0408c647f9993fdc37e8fc
danilkinkin <d.zahvatkin@ticketscloud.com>
2023-04-11 21:46:46 +0400

    fix: fixed split emojis in `AnimatedNumber`

--- app/src/main/java/com/danilkinkin/buckwheat/base/AnimatedNumber.kt
+++ app/src/main/java/com/danilkinkin/buckwheat/base/AnimatedNumber.kt
@@ -6,6 +6,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.text.TextStyle
+import java.text.BreakIterator
 
 data class CharState(val preview: String, val current: String)
 
@@ -16,19 +17,34 @@ fun AnimatedNumber(
     style: TextStyle = MaterialTheme.typography.displayLarge,
 ) {
 
-    var previewsValue by remember { mutableStateOf("") }
+    var previewsValue by remember { mutableStateOf<List<String>>(emptyList()) }
     var blocks by remember { mutableStateOf<List<CharState>>(emptyList()) }
 
     DisposableEffect(value) {
-        val length = value.length.coerceAtLeast(previewsValue.length)
+        var splittedValue = emptyList<String>().toMutableList()
+        val it = BreakIterator.getCharacterInstance()
+        it.setText(value)
+        var count = 0
+
+        var start = 0
+        var end = it.next()
+        while (end != BreakIterator.DONE) {
+            splittedValue.add(value.substring(start, end))
+
+            start = end
+            end = it.next()
+            count++
+        }
+
+        val length = splittedValue.size.coerceAtLeast(previewsValue.size)
 
         var newBlocks: MutableList<CharState> = emptyList<CharState>().toMutableList()
 
         for (i in 0 .. length) {
             newBlocks.add(
                 CharState(
-                    preview = previewsValue.getOrElse(previewsValue.length - i) { ' ' }.toString(),
-                    current = value.getOrElse(value.length - i) { ' ' }.toString(),
+                    preview = previewsValue.getOrElse(previewsValue.size - i) { " " },
+                    current = splittedValue.getOrElse(splittedValue.size - i) { " " },
                 )
             )
         }
@@ -36,7 +52,7 @@ fun AnimatedNumber(
         newBlocks = newBlocks.asReversed()
 
         blocks = newBlocks
-        previewsValue = value
+        previewsValue = splittedValue
 
         onDispose {  }
     }
