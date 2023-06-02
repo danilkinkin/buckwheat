a3d62ea5ad24829341885bca3eaa48c83f24c549
danilkinkin <danilkinkin@gmail.com>
2023-06-02 22:44:18 +0200

    bump: update gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:8.0.1")
+        classpath("com.android.tools.build:gradle:8.0.2")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.45")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
         classpath("com.diffplug.spotless:spotless-plugin-gradle:6.16.0")
