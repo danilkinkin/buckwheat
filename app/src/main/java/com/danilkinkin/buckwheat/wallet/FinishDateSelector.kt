d71d836cbfa5d67ab9edd22d183b3eec11023148
danilkinkin <danilkinkin@gmail.com>
2023-08-05 21:38:59 +0200

    fix: change cancel icon in finish date selector

--- app/src/main/java/com/danilkinkin/buckwheat/wallet/FinishDateSelector.kt
+++ app/src/main/java/com/danilkinkin/buckwheat/wallet/FinishDateSelector.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -72,12 +73,15 @@ private fun FinishDateSelectorTopAppBar(
 ) {
     Surface {
         Column() {
-            Row(Modifier.fillMaxWidth().padding(8.dp)) {
+            Row(
+                Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)) {
                 IconButton(
                     onClick = { onBackPressed() }
                 ) {
                     Icon(
-                        imageVector = Icons.Filled.ArrowBack,
+                        painter = painterResource(R.drawable.ic_close),
                         contentDescription = null,
                     )
                 }
