d009b09900c36bfc5441bd99c02b4672d29ba676
danilkinkin <danilkinkin@gmail.com>
2023-08-16 23:53:09 +0200

    bump: bump target version to 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 }
 
 android {
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.danilkinkin.buckwheat"
         minSdk = 26
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 18
         versionName = "3.1.0"
         testInstrumentationRunner = "com.danilkinkin.buckwheat.CustomTestRunner"
@@ -69,38 +69,38 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.activity:activity-compose:1.7.2")
-    implementation("androidx.compose.runtime:runtime:1.4.3")
-    implementation("androidx.compose.foundation:foundation:1.4.3")
-    implementation("androidx.compose.foundation:foundation-layout:1.4.3")
-    implementation("androidx.compose.ui:ui-util:1.4.3")
+    implementation("androidx.compose.runtime:runtime:1.5.0")
+    implementation("androidx.compose.foundation:foundation:1.5.0")
+    implementation("androidx.compose.foundation:foundation-layout:1.5.0")
+    implementation("androidx.compose.ui:ui-util:1.5.0")
     implementation("androidx.compose.material3:material3-window-size-class:1.1.1")
-    implementation("androidx.compose.animation:animation:1.4.3")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.4.3")
-    implementation("androidx.compose.runtime:runtime-livedata:1.4.3")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.4.3")
+    implementation("androidx.compose.animation:animation:1.5.0")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.5.0")
+    implementation("androidx.compose.runtime:runtime-livedata:1.5.0")
     implementation("androidx.compose.material3:material3:1.1.1")
-    implementation("androidx.compose.material:material:1.4.3")
+    implementation("androidx.compose.material:material:1.5.0")
     implementation("androidx.datastore:datastore-preferences:1.0.0")
     implementation("androidx.recyclerview:recyclerview:1.3.1")
     implementation("com.google.accompanist:accompanist-systemuicontroller:0.25.1")
     implementation("androidx.room:room-runtime:2.5.2")
-    kapt("androidx.room:room-compiler:2.5.2")
     implementation("androidx.room:room-ktx:2.5.2")
     implementation("androidx.room:room-paging:2.5.2")
-    implementation("com.google.dagger:dagger:2.46.1")
-    kapt("com.google.dagger:dagger-compiler:2.46.1")
+    implementation("com.google.dagger:dagger:2.47")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
     implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
     implementation("com.google.dagger:hilt-android:2.46.1")
-    implementation("androidx.glance:glance-appwidget:1.0.0-beta01")
-    debugImplementation("com.google.android.glance.tools:appwidget-viewer:0.2.2")
+    implementation("androidx.glance:glance-appwidget:1.0.0-rc01")
     implementation("org.apache.commons:commons-csv:1.10.0")
-    kapt("com.google.dagger:hilt-android-compiler:2.46.1")
-    kapt("androidx.hilt:hilt-compiler:1.0.0")
     implementation("io.coil-kt:coil-compose:2.3.0")
     implementation("androidx.core:core-splashscreen:1.0.1")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.4.3")
+    kapt("com.google.dagger:dagger-compiler:2.46.1")
+    kapt("androidx.room:room-compiler:2.5.2")
+    kapt("com.google.dagger:hilt-android-compiler:2.46.1")
+    kapt("androidx.hilt:hilt-compiler:1.0.0")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.5.0")
+    debugImplementation("com.google.android.glance.tools:appwidget-viewer:0.2.2")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.5.0")
     androidTestImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test:core:1.5.0")
     androidTestImplementation("androidx.test:runner:1.5.2")
@@ -108,8 +108,8 @@ dependencies {
     androidTestImplementation("androidx.test:rules:1.5.0")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.1")
-    androidTestImplementation("androidx.compose.ui:ui-test:1.4.3")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.4.3")
+    androidTestImplementation("androidx.compose.ui:ui-test:1.5.0")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.5.0")
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:1.1.5")
     androidTestImplementation("com.google.dagger:hilt-android-testing:2.46.1")
     kaptAndroidTest("com.google.dagger:hilt-android-compiler:2.46.1")
