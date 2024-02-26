11eb71da09a45d87ed2be5032e1fc859f5caf9c3
danilkinkin <hello@danilkinkin.com>
2024-02-26 19:27:04 +0600

    bump: update app version to 4.5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.danilkinkin.buckwheat"
         minSdk = 26
         targetSdk = 34
-        versionCode = 23
-        versionName = "4.3.0"
+        versionCode = 24
+        versionName = "4.5.0"
         testInstrumentationRunner = "com.danilkinkin.buckwheat.CustomTestRunner"
         javaCompileOptions {
             annotationProcessorOptions {
