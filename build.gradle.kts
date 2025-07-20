cd21e6ac207c482d40d267d9845f5e1fd8e15a8e
danilkinkin <hello@danilkinkin.com>
2025-07-20 10:37:23 +0200

    bump: bump dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,11 +18,11 @@ subprojects {
 }
 
 plugins {
-    id("com.android.application") version "8.11.0" apply false
-    id("com.android.library") version "8.11.0" apply false
+    id("com.android.application") version "8.11.1" apply false
+    id("com.android.library") version "8.11.1" apply false
     id("org.jetbrains.kotlin.android") version "2.2.0" apply false
-    id("com.google.dagger.hilt.android") version "2.56.2" apply false
-    id("com.diffplug.spotless") version "7.0.4"
+    id("com.google.dagger.hilt.android") version "2.57" apply false
+    id("com.diffplug.spotless") version "7.1.0"
     id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin") version "2.0.1" apply false
     id("com.github.ben-manes.versions") version "0.52.0"
     id("com.google.devtools.ksp") version "2.2.0-2.0.2" apply false
